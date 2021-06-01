121a0c4eb97e94278957e0a526419ad2b1f4f84a
rizkyhamdana07 <rizky.hamdana07@gmail.com>
2021-06-01 10:20:32 +0800

    Add Leak Canary

--- app/src/main/java/com/rizkyhamdana/movietvcatalogue/home/HomeActivity.kt
+++ app/src/main/java/com/rizkyhamdana/movietvcatalogue/home/HomeActivity.kt
@@ -1,9 +1,14 @@
 package com.rizkyhamdana.movietvcatalogue.home
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.rizkyhamdana.movietvcatalogue.R
@@ -12,6 +17,7 @@ import com.rizkyhamdana.movietvcatalogue.databinding.ActivityHomeBinding
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var broadcastReceiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,4 +33,37 @@ class HomeActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         navView.setupWithNavController(navController)
     }
+
+
+    private fun registerBroadCastReceiver() {
+        broadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                when (intent.action) {
+                    Intent.ACTION_POWER_CONNECTED -> {
+                        Log.d(context.toString(), getString(R.string.power_connected))
+                    }
+                    Intent.ACTION_POWER_DISCONNECTED -> {
+                        Log.d(context.toString(), getString(R.string.power_disconnected))
+                    }
+                }
+            }
+        }
+        val intentFilter = IntentFilter()
+        intentFilter.apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+        }
+        registerReceiver(broadcastReceiver, intentFilter)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerBroadCastReceiver()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
+    }
+
 }
